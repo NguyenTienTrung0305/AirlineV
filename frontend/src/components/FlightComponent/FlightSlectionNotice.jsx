347c7070f3e1188cf714c2137ced69c8a15eb882
Nguyen Tien Trung <nguyentientrung204@gmail.com>
2025-03-19 23:13:00 +0700

    add new flight component

---
+++ frontend/src/components/FlightComponent/FlightSlectionNotice.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export function FlightSelectionNotice() {
+    return (
+        <div className="bg-blue-100 border-l-4 border-blue-500 text-blue-700 p-4 mb-4 rounded ">
+            <p className="font-bold">
+                Chọn chuyến bay đi
+            </p>
+            <p>
+                Vui lòng chọn chuyến bay của bạn
+            </p>
+        </div>
+    )
+}
