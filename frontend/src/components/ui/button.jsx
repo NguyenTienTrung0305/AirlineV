4460b50631ae56be9337df719a7c89b8d9a1bed0
Nguyen Tien Trung <nguyentientrung204@gmail.com>
2025-04-03 22:07:40 +0700

    update component

--- frontend/src/components/ui/button.jsx
+++ frontend/src/components/ui/button.jsx
@@ -7,13 +7,14 @@ import { Slot } from "@radix-ui/react-slot";
 
 
 const ButtonVariances = cva(
-    ["inline-flex items-center justify-center gap-2 rounded-md", // căn giữa theo chiều ngang
+    [
+        "inline-flex items-center justify-center gap-2 rounded-md", // căn giữa theo chiều ngang
         "text-sm front-medium",
         "ring-offet-background",
         "transition-colors",
         "focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-0",
         "disabled:pointer-events-none disabled:opacity-50",
-        "[&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0" // Nếu có icon SVG bên trong button, nó không bị ảnh hưởng bởi click và có kích thước chuẩn
+        "[&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0" // Nếu có icon SVG là con của button, nó không bị ảnh hưởng bởi click và có kích thước cố định (không bị co lại)
     ],
 
     // định nghĩa các kiểu button khác nhau
