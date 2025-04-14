b2dd774a979b1e515ee16ffb84e4da6c508be33c
Nguyen Tien Trung <nguyentientrung204@gmail.com>
2025-04-14 21:15:37 +0700

    update axios

--- frontend/src/util/api.js
+++ frontend/src/util/api.js
@@ -4,9 +4,21 @@ import axios from "./axiosCustom"
 const loginUserApi = (email, password) => {
     const BASE_URL_API = '/api/login' // fetch to backend localhost://3000/api/login
     const data = { email: email, password: password }
-    return axios.post(URL_API, data)
+    return axios.post(BASE_URL_API, data)
+}
+
+const signupUserApi = (formData) => {
+    const BASE_URL_API = '/api/signup' // fetch to backend localhost://3000/api/signup
+    const data = {
+        firstName: formData.firstName,
+        lastName: formData.lastName,
+        email: formData.email,
+        password: formData.password,
+    }
+    return axios.post(BASE_URL_API, data)
 }
 
 export {
-    loginUserApi
+    loginUserApi,
+    signupUserApi
 }
--- frontend/src/util/axiosCustom.js
+++ frontend/src/util/axiosCustom.js
@@ -1,7 +1,10 @@
 import axios from 'axios'
 
 const instance = axios.create({
-    baseURL: process.env.PUBLIC_API_BASE_URL
+    baseURL: process.env.PUBLIC_API_BASE_URL,
+    headers: {
+        'Content-Type': 'application/json',
+    },
 })
 
 instance.interceptors.request.use(
@@ -12,6 +15,10 @@ instance.interceptors.request.use(
         if (localStorage.getItem('token')) {
             config.headers.Authorization = `Bearer ${localStorage.getItem('token')}`;
         }
+
+        //get roleadmin 
+        const isAdmin = localStorage.getItem('admin') === 'true'
+        config.headers.admin = isAdmin ? 'true' : 'false'
         return config;
     },
     function (error) {
@@ -21,12 +28,13 @@ instance.interceptors.request.use(
 );
 
 instance.interceptors.response.use(
-    function (response) {
-        return response.data;
-    },
-    function (err) {
-        if (err?.response?.data) return err?.response?.data;
-        return Promise.reject(err);
+    (response) => response.data,
+    (error) => {
+        if (error.response?.status === 401) {
+            localStorage.removeItem('token')
+            window.dispatchEvent(new Event("storage")) // sync out
+        }
+        return Promise.reject(error)
     }
 );
 
