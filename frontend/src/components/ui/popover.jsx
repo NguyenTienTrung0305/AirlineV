60068fe5355d3155e9dbc974e53b552cb131ba2e
Nguyen Tien Trung <nguyentientrung204@gmail.com>
2025-04-21 23:53:59 +0700

    toast component

--- frontend/src/components/ui/popover.jsx
+++ frontend/src/components/ui/popover.jsx
@@ -5,11 +5,11 @@ import { cn } from "@/lib/utils"
 const Popover = PopoverPrimitive.Root
 const PopoverTrigger = PopoverPrimitive.Trigger
 
-const PopoverContent = React.forwardRef(({ className, sideoffset = 4, ...props }, ref) => (
+const PopoverContent = React.forwardRef(({ className, sideOffset = 4, ...props }, ref) => (
     <PopoverPrimitive.Portal>
         <PopoverPrimitive.Content
             ref={ref}
-            sideoffset={sideoffset}
+            sideOffset={sideOffset}
             className={
                 cn(
                     "z-50 w-72 rounded-md border bg-popover p-4 text-popover-foreground shadow-md outline-none",
