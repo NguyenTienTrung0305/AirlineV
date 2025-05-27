3b2e32b8d2ef9abc763de929a0aa3a9ec9ebfb54
Nguyen Tien Trung <nguyentientrung204@gmail.com>
2025-05-27 22:54:57 +0700

    update router

--- backend/src/routes/flight/index.js
+++ backend/src/routes/flight/index.js
@@ -1,7 +1,11 @@
 import express from 'express'
-import { createNewFlight } from '../../controller/flights/Flight.controller'
+import { createNewFlight, getFlightSuggesion, getTypeSeats, lockSeat, searchFlightMock } from '../../controller/flights/Flight.controller.js'
 
 const router = express.Router()
 router.post('/new', createNewFlight)
+router.get('/search', searchFlightMock)
+router.get('/suggesion', getFlightSuggesion)
+router.get('/typeSeat', getTypeSeats)
+router.get('/lock-seat', lockSeat)
 
 export default router
--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -7,13 +7,15 @@ import { isAdminRoute, verifySessionAndCSRF } from '../middleware/auth.middlewar
 import flightsRouters from './flight/index.js'
 
 const initWebRoutes = (app) => {
-    app.use("/api/auth", authRouters)
+    app.use("/admin/auth", authRouters)
     app.use("/api/login", loginRouters)
     app.use("/api/user", userRouters)
     app.use("/api/admin", adminRouters)
 
     app.use("/api/typeSeats", verifySessionAndCSRF, isAdminRoute, createTypeSeatRouters)
     app.use("/api/admin/flights", verifySessionAndCSRF, isAdminRoute, flightsRouters)
+
+    app.use("/api/flights", flightsRouters)
 
     app.use("/", (req, res) => res.send("Hello World"))
     return app
