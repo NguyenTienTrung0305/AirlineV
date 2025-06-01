17e7155dabec6dab43457c16a874d95db0975054
Nguyen Tien Trung <nguyentientrung204@gmail.com>
2025-06-01 14:18:13 +0700

    test fix cors

--- backend/server.js
+++ backend/server.js
@@ -7,9 +7,11 @@ import initWebRoutes from './src/routes/index.js'
 
 const app = express()
 
-const allowedOrigins = process.env.ALLOWED_ORIGINS
-  ? process.env.ALLOWED_ORIGINS.split(',')
-  : ['http://localhost:3000'] // Chỉ cho phép origin của frontend
+if (process.env.NODE_ENV === 'development') {
+    allowedOrigins = 'http://localhost:3000'
+} else {
+    allowedOrigins = 'https://airline-v.vercel.app/'
+}
 
 app.use(cors({
     origin: (origin, callback) => {
@@ -35,10 +37,10 @@ app.use(cookieParser())
 // Khi một người dùng mới truy cập ứng dụng (hoặc phiên của họ hết hạn), express-session sẽ tạo một Session ID duy nhất cho họ dựa trên secret và bộ mã hóa
 // Nên kết nối với redis hoặc database trong môi trường production
 // Khác với Session Cookie (Cookie ở Frontend)
-    // + Đây là một cookie HTTP được gửi từ server về trình duyệt (frontend) của người dùng
-    // + Trong các request tiếp theo từ trình duyệt đó đến server, cookie này sẽ được tự động gửi đi trong header Cookie. 
+// + Đây là một cookie HTTP được gửi từ server về trình duyệt (frontend) của người dùng
+// + Trong các request tiếp theo từ trình duyệt đó đến server, cookie này sẽ được tự động gửi đi trong header Cookie. 
 // Server (thông qua express-session middleware) sẽ sử dụng Session ID được gửi trong cookie được cấu hình trong express-session để xác định phiên của người dùng và truy 
-    // cập dữ liệu phiên đã được lưu trữ ở backend (req.sesison.user, req.sesison.role,...)
+// cập dữ liệu phiên đã được lưu trữ ở backend (req.sesison.user, req.sesison.role,...)
 app.use(session({
     secret: process.env.SECRET_KEY,
     resave: false,
@@ -62,5 +64,5 @@ const port = process.env.PORT || 8080
 const host = process.env.HOST || '0.0.0.0'
 
 app.listen(port, host, () => {
-  console.log(`Server running on http://${host}:${port}`)
+    console.log(`Server running on http://${host}:${port}`)
 })
